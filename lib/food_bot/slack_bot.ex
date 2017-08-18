b6c4a91666ff22474b4ba4d361e08030f7fc58de
Pedro Chambino <pchambino@gmail.com>
2017-08-18 13:12:23 +0100

    Extract order command into module

--- lib/food_bot/slack_bot.ex
+++ lib/food_bot/slack_bot.ex
@@ -1,9 +1,6 @@
 defmodule FoodBot.SlackBot do
   use Slack
 
-  alias FoodBot.{Repo, Order}
-  import FoodBot.CommandHelpers
-
   def start_link() do
     Slack.Bot.start_link(__MODULE__, %{}, Application.get_env(:food_bot, :slack_bot_token))
   end
@@ -35,36 +32,8 @@ defmodule FoodBot.SlackBot do
   def handle_command("join_event", name, state) do
     FoodBot.JoinEventCommand.execute(name, state)
   end
-  def handle_command("order", nil, state) do
-    { "Sorry, you didn't provide an order.", state }
-  end
-  def handle_command("order", text, state = %{event: event, user: %{name: user_name}}) do
-    case Enum.count event.food_sources do
-      0 -> raise "TODO"
-      1 ->
-        {status, _} = Repo.insert %Order{
-          event: event,
-          food_source: Enum.at(event.food_sources, 0),
-          order: text,
-          user_name: user_name
-        }
-
-        case status do
-          :ok -> { "Your order has been taken. Thank you.", state }
-          :error -> { "Something went wrong while saving your order. :(", state }
-        end
-
-      _ -> raise "TODO"
-    end
-  end
-  def handle_command("order", _, state) do
-    {
-      """
-      Sorry, you need to join an event first. Is it one of these?
-      #{latest_events_text()}
-      """,
-      state
-    }
+  def handle_command("order", text, state) do
+    FoodBot.OrderCommand.execute(text, state)
   end
   def handle_command(_, _, state) do
     {
