1f70f7957ac81b598403fa9ecce3454455e3709b
Pedro Chambino <pchambino@gmail.com>
2017-08-18 13:34:21 +0100

    Extract order command tests into module

--- test/slack_bot_test.exs
+++ test/slack_bot_test.exs
@@ -9,31 +9,4 @@ defmodule FoodBot.SlackBotTest do
       "Available commands are `join_event` and `current_event`", %{}
     }
   end
-
-  test "order command: no event joined" do
-    assert SlackBot.handle_command("order", "Regular Funghi Lasagna") == {
-      """
-      Sorry, you need to join an event first. Is it one of these?
-       - Design Lunch
-       - Tech Lunch
-      """,
-      %{}
-    }
-  end
-
-  test "order command: no order" do
-    assert SlackBot.handle_command("order") == {
-      "Sorry, you didn't provide an order.", %{}
-    }
-  end
-
-  test "order command: single food source" do
-    {_, state} = SlackBot.handle_command("join_event", "Design Lunch")
-
-    state = Map.put state, :user, %{name: "pedro"}
-
-    assert SlackBot.handle_command("order", "Regular Funghi Lasagna", state) == {
-      "Your order has been taken. Thank you.", state
-    }
-  end
 end
